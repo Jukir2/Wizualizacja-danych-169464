a3d8a8f021bc90110eee81247471a96b4af2a14e
jasdsj <jasds2002@gmail.com>
2023-03-08 12:32:29 +0100

    #2

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+# zmienne
+a = 'napis'
+print(a)
+print(type(a))
+b = 5
+print(b)
+c = 7.5
+print(c)
+print(b + c)
+
+# listy
 listy = ['2', 4, 5, 2, 3, 5.4, [2, 3, 4]]
 print(listy)
 listy.append(4)
@@ -22,3 +33,49 @@ listy.reverse()
 print(listy)
 listy.sort()
 print(listy)
+
+# slownik
+slownik = {"kamienie": 2, "drewno": 7, "złoto": 27}
+print(slownik)
+print(slownik.keys())
+print(slownik.values())
+print(len(slownik))
+
+print('a = %(zm)d' % {'zm': 12})
+print('a = {}'.format(12))
+#
+# napis = input('wprowadz liczbe: ')
+# print(napis)
+# print(type(napis))
+# napis = int(napis)
+# print(type(napis))
+
+# instrukcja warunkowa
+# if warunek:
+# instrukcje
+# elif warunek:
+# instrukcje
+# else:
+# instrukcje
+
+x = input('podaj x: ')
+y = input('podaj y: ')
+z = input('podaj z: ')
+t = input('podaj t: ')
+if x > y:
+    print(x + ' jest wieksze od ' + y)
+elif x < y:
+    print(x + ' jest mniejsze od ' + y)
+else:
+    print(x + ' jest równe ' + y)
+
+if (x > y) & (z > t):
+    print("x jest wieksze od y i z jest wieksze od t")
+
+for i in range(1, 6, 1):
+    print(i)
+else:
+    print('koniec petli for')
+
+for j in range(len(listy)):
+    print(j, listy[j])
