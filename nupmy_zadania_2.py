be16c4f6c9379b4b80792f1f973ccfd9d5b7cf39
Jakub Michalski <48523914+Jukir2@users.noreply.github.com>
2023-04-19 12:50:28 +0200

    Create nupmy_zadania_2.py

---
+++ nupmy_zadania_2.py
@@ -0,0 +1,76 @@
+import numpy as np
+
+# a=np.array([10, 20, 30, 40])
+# b=np.array([1, 2, 3, 4])
+# c = a + b
+# print(c)
+
+#
+# tab = np.array([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]])
+# print(tab.sum())
+# print(tab.sum(axis=0))
+# print(tab.sum(axis=1))
+# print(tab.cumsum(axis=1))
+#
+# tab1 = np.array([[2, 1, 3], [-1, 2, 4]])
+# tab2 = np.array([[1, 3], [2, -2], [-1, 4]])
+# # tab5 = np.array([[1, 3], [2, -2], [-1, 4], [2,1]])
+# tab3 = np.dot(tab1, tab2)
+# print(tab3)
+# tab4 = tab1.dot(tab2)
+# print(tab4)
+# # tab4 = tab1.dot(tab5)
+#
+#
+# a = np.arange(6).reshape((3, 2))
+# print(a)
+# for b in a:
+#     print(b)
+# for c in range(0, a.shape[0], 1):
+#     for d in range(9, a.shape[1], 1):
+#         print(a[c][d])
+# for e in a.flat:
+#     print(e)
+
+# a = np.arange(6)
+# print(a)
+# print(a.shape)
+# b = a.reshape((2, 3))
+# print(b)
+# print(b.shape)
+# c = a.reshape((3, 2))
+# print(c)
+# print(c.shape)
+# d = a.reshape((6, 1))
+# print(d)
+# print(d.shape)
+#
+# e = c.ravel()
+# print(e)
+#
+# print(c)
+# f = c.T
+# print(f)
+#
+# a = np.array([0, 1, 2])
+# b = np.array([0, 1, 2])
+#
+# print(a)
+# print(b)
+# c = np.dot(a, b)
+# print(c)
+#
+# d = a * b
+# print(d)
+
+#zad1
+# a=np.array([2,3,4])
+# b=np.array([4,5,6])
+# c = a.dot(b)
+# d= np.dot(a,b)
+# print(c)
+# print(d)
+
+#zad2
+a = np.array([[2,3,4],[5,6,7],[8,9,0]])
+b = np.array([[2,3,4,1],[5,6,7,1],[8,9,0,1],[2,3,4,1]])
